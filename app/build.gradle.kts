5cc76935c2f42cdf6b512ace6653c78250bbba80
Jaideep <jaideepindia99@gmail.com>
2025-05-30 04:15:45 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.jai.mario"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jai.mario"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("release") {
+            // These four ENV vars will be provided by GitHub Actions
+            storeFile = file(System.getenv("KEYSTORE_PATH"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias    = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        debug {
+            // debug remains as-is
+        }
+        release {
+            signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation 'org.videolan.android:libvlc-all:3.5.1'
+}
